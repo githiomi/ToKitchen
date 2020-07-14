93c1932aa579bf3da6c2ed6a8d1c7a7b41af9b0d
githiomi <danielgithiomi@gmail.com>
2020-07-14 15:00:21 +0300

    Implementing the side panel idea for efficiency

---
+++ app/src/main/java/org/githiomi/tokitchen/ui/AltActivity.java
@@ -0,0 +1,14 @@
+package org.githiomi.tokitchen.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AltActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alt);
+    }
+}
