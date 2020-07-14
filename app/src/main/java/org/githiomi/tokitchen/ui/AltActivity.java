e8892356b656385898db908e583978bb1a70a9d9
githiomi <danielgithiomi@gmail.com>
2020-07-14 18:29:43 +0300

    Creating the design for a single main category item in the panel

--- app/src/main/java/org/githiomi/tokitchen/ui/AltActivity.java
+++ app/src/main/java/org/githiomi/tokitchen/ui/AltActivity.java
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import org.githiomi.tokitchen.R;
+
 public class AltActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alt);
+
     }
 }
