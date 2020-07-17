b1fad45ab20ddcabcdac09dcbcab0676663f5812
githiomi <danielgithiomi@gmail.com>
2020-07-17 16:29:32 +0300

    I can successfully click on coffee and espressos and get the types of the coffee (no sizes)

---
+++ app/src/main/java/org/githiomi/tokitchen/adapters/BaristaTypeAdapter.java
@@ -0,0 +1,94 @@
+package org.githiomi.tokitchen.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.githiomi.tokitchen.R;
+import org.githiomi.tokitchen.models.Barista.BaristaType;
+
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class BaristaTypeAdapter extends RecyclerView.Adapter<BaristaTypeAdapter.BaristaTypeViewHolder> {
+
+//    TAG
+    private static final String TAG = BaristaTypeAdapter.class.getSimpleName();
+
+//    Local Variables
+    // For the context
+    private Context context;
+    // For the list of barista types
+    private List<BaristaType> baristaTypeList;
+
+    public BaristaTypeAdapter(List<BaristaType> baristaTypeList, Context context) {
+        this.context = context;
+        this.baristaTypeList = baristaTypeList;
+    }
+
+    @NonNull
+    @Override
+    public BaristaTypeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View mainView = LayoutInflater.from(parent.getContext()).inflate(R.layout.meal_order_item, parent, false);
+        BaristaTypeViewHolder baristaTypeViewHolder = new BaristaTypeViewHolder(mainView);
+        return baristaTypeViewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull BaristaTypeViewHolder holder, int position) {
+        holder.bindBaristaType(baristaTypeList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return baristaTypeList.size();
+    }
+
+    public class BaristaTypeViewHolder extends RecyclerView.ViewHolder
+                                       implements View.OnClickListener {
+
+        // Widgets
+        @BindView(R.id.mealName) TextView wMealName;
+        @BindView(R.id.ivButtonAddOrder) ImageView wButtonAddOrder;
+
+        public BaristaTypeViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            // Binding views using butter knife
+            ButterKnife.bind(this, itemView);
+
+            // Setting an onclick listener for the item view
+            wButtonAddOrder.setOnClickListener(this);
+        }
+
+        // Method for binding the views
+        public void bindBaristaType(BaristaType baristaType){
+
+            wMealName.setText(baristaType.getBaristaType());
+
+        }
+
+        // Method for the item view on click listener
+        @Override
+        public void onClick(View view) {
+
+            int currentPosition = getAdapterPosition();
+
+            if ( view == wButtonAddOrder ){
+
+                Toast.makeText(context, "Order: " + baristaTypeList.get(currentPosition).getBaristaType(), Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+    }
+}
--- app/src/main/java/org/githiomi/tokitchen/fragments/MealOrderFragment.java
+++ app/src/main/java/org/githiomi/tokitchen/fragments/MealOrderFragment.java
@@ -1,14 +1,33 @@
 package org.githiomi.tokitchen.fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import org.githiomi.tokitchen.R;
+import org.githiomi.tokitchen.adapters.BaristaTypeAdapter;
+import org.githiomi.tokitchen.adapters.MainCategoryAdapter;
+import org.githiomi.tokitchen.models.Barista.BaristaCategory;
+import org.githiomi.tokitchen.models.Barista.BaristaSizes;
+import org.githiomi.tokitchen.models.Barista.BaristaType;
+import org.githiomi.tokitchen.models.Constants;
+import org.githiomi.tokitchen.models.MainCategory;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +38,26 @@ public class MealOrderFragment extends Fragment {
 
 //    TAG
     private static final String TAG = MealOrderFragment.class.getSimpleName();
+
+//    Widgets
+    // Recycler View
+    @BindView(R.id.mealsRecyclerView) RecyclerView wMealsRecyclerView;
+
+//    Local variables
+    // For the adapter
+    private BaristaTypeAdapter baristaTypeAdapter;
+    // For the main categories
+    private List<String> categoryNames;
+    // For the list of main category objects
+    private List<MainCategory> mainCategoryList;
+    // For the list of sub categories
+    private List<BaristaCategory> baristaSubCategoryList;
+    // For the other list
+    private List<String> subCategoryList;
+    // For the list of barista types
+    private List<BaristaType> baristaTypeList;
+    // For the list of barista sizes and prices
+    private List<BaristaSizes> baristaSizesList;
 
     public MealOrderFragment() {
         // Required empty public constructor
@@ -42,6 +81,139 @@ public class MealOrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_meal_order, container, false);
+        View mainView = inflater.inflate(R.layout.fragment_meal_order, container, false);
+
+        // Binding using butter knife
+        ButterKnife.bind(this, mainView);
+
+        // Method that will get the data from source.json
+        extractData();
+
+        return mainView;
     }
+
+//    Method to get data from source
+    public void extractData() {
+        Log.d(TAG, "extractData: init");
+
+        try {
+//            Locating the file to read the data
+            JSONObject jsonObject = new JSONObject(Constants.loadJsonFromAssets(getActivity()));
+
+//            The key for the content I want to get
+            String key = "java";
+            JSONArray mealCategories = jsonObject.getJSONArray(key);
+
+            int mealCategoriesCount = mealCategories.length();
+
+            // init the lists
+            categoryNames = new ArrayList();
+            mainCategoryList = new ArrayList<>();
+
+            for (int i = 0; i < mealCategoriesCount; i += 1) {
+
+                JSONObject mealCategory = (JSONObject) mealCategories.get(i);
+                String categoryName = mealCategory.getString("name");
+
+                // Breakfast, Lunch & Dinner, Barista Corner, Drinks
+
+                categoryNames.add(categoryName);
+
+                // To get the categories inside the 4 main categories
+                String mealCategoriesKey = "categories";
+                JSONArray breakfastCategories = mealCategory.getJSONArray(mealCategoriesKey);
+                int length = breakfastCategories.length();
+
+                // Initializing the array list
+                subCategoryList = new ArrayList();
+
+                for (int d = 0; d < length; d += 1) {
+
+                    JSONObject mealSubCategories = (JSONObject) breakfastCategories.get(d);
+
+                    String forTheSubCategories = "categoryName";
+                    String bCategoryName = mealSubCategories.getString(forTheSubCategories);
+                    Log.d(TAG, categoryName + " categories: " + bCategoryName);
+
+                    if (bCategoryName.equals("Coffee & Espresso")) {
+
+                        baristaSubCategoryList = new ArrayList();
+
+                        // To get the meals in the sub category
+                        String forTheMealInSubCategory = "meals";
+                        JSONArray coffeeAndEspressos = mealSubCategories.getJSONArray(forTheMealInSubCategory);
+
+                        int lengthOfCoffeeAndEsp = coffeeAndEspressos.length();
+
+                        baristaTypeList = new ArrayList();
+                        for (int z = 0; z < lengthOfCoffeeAndEsp; z += 1) {
+
+                            JSONObject coffeeAndEspresso = (JSONObject) coffeeAndEspressos.get(z);
+
+                            String toGetCoffeeAndEspresso = "mealName";
+                            String coffeeAndE = coffeeAndEspresso.getString(toGetCoffeeAndEspresso);
+
+                            Log.d(TAG, "readJsonFile: Coffee And Espresso: --------------- " + coffeeAndE);
+
+
+                            String forTheSizesOfCAE = "sizes";
+                            JSONArray forTheSizesOfCAEArray = coffeeAndEspresso.getJSONArray(forTheSizesOfCAE);
+
+                            int sizeOfCA = forTheSizesOfCAEArray.length();
+
+                            baristaSizesList = new ArrayList();
+                            for (int x = 0; x < sizeOfCA; x += 1) {
+
+                                JSONObject theSizes = (JSONObject) forTheSizesOfCAEArray.get(x);
+
+                                String size = theSizes.getString("sizeName");
+                                int sizePrice = theSizes.getInt("sizePrice");
+
+                                // Creating a new barista sizes list
+                                BaristaSizes baristaSize = new BaristaSizes(size, sizePrice);
+                                baristaSizesList.add(baristaSize);
+
+                                Log.d(TAG, "readJsonFile: One of the sizes for the " + coffeeAndE + " is: " + size + " that costs " + sizePrice);
+
+                            }
+
+                            // Creating the barista type objects
+                            BaristaType baristaType = new BaristaType(coffeeAndE, baristaSizesList);
+                            baristaTypeList.add(baristaType);
+
+                        }
+                    }
+                }
+
+                Log.d(TAG, "readJsonFile: One of the categories is: ---------- " + categoryName);
+
+
+                // Method to pass the meals to the adapter
+                Log.d(TAG, "readJsonFile: The array passed to the barista type adapter is: --------- " + baristaTypeList);
+                passToAdapter(baristaTypeList);
+
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+    }
+
+//    Method that will pass the items
+    private void passToAdapter(List<BaristaType> baristaTypeListForAdapter) {
+
+        Log.d(TAG, "passToAdapter: init");
+
+        Context context = getContext();
+
+        baristaTypeAdapter = new BaristaTypeAdapter(baristaTypeListForAdapter, context);
+
+        wMealsRecyclerView.setAdapter(baristaTypeAdapter);
+        wMealsRecyclerView.setLayoutManager(new LinearLayoutManager(context));
+
+        wMealsRecyclerView.setHasFixedSize(true);
+        baristaTypeAdapter.notifyDataSetChanged();
+
+    }
+
 }
